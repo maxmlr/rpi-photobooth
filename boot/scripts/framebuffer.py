c430635a6ee8461ebeebd80b623e4ff8922737a1
maxmlr <miller.deutschland@gmail.com>
2020-04-18 02:45:34 -0400

    fixed framebuffer for boot splash screen

--- boot/scripts/framebuffer.py
+++ boot/scripts/framebuffer.py
@@ -8,6 +8,10 @@ from FBpyGIF import fb
 from argparse import ArgumentParser
 from helpers import source
 
+# with imagemagick
+# convert -resize 1024x600 -background black -gravity center -extent 1024x600 /opt/photobooth/img/boot.png [bgr|bgra]:/dev/fb0
+# with FBpyGIF
+# python3 -m FBpyGIF /opt/photobooth/img/boot.png
 
 def resize(image, width, height):
     img = Image.open(image)
@@ -26,7 +30,7 @@ def crop(image, left, top, right, bottom):
 
 
 def getFrameBuffer(image):
-    BIT_DEPTH = 8
+    BIT_DEPTH = 24
     FRAME_BUFFER = 0
     fb.ready_fb(BIT_DEPTH, FRAME_BUFFER)
     fb.show_img(fb.ready_img(image))
@@ -40,9 +44,8 @@ if __name__ == "__main__":
     photobooth_config = source('/boot/photobooth.conf')
 
     img_path = Path(args.image)
-    img_preprocessed_path = img_path.parents[0] / f'{img_path.stem}_preprocessed.png'
+    img_preprocessed_path = img_path.parents[0] / f'.{img_path.stem}.png'
     if  not img_preprocessed_path.exists():
-        # img = resize(img_path, int(photobooth_config['DISPLAY_RESOLUTION_X']), int(photobooth_config['DISPLAY_RESOLUTION_Y']))
         img = resize(img_path, int(photobooth_config['DISPLAY_RESOLUTION_X']), int(photobooth_config['DISPLAY_RESOLUTION_Y']))
     else:
         img = Image.open(img_preprocessed_path)
