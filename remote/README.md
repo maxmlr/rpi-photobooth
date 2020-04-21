248a3181d68bb64c10c6994859af6511aa6a7e61
maxmlr <miller.deutschland@gmail.com>
2020-04-20 22:43:03 -0400

    updated arduino codeadded both wemos-d1 / esp-01S

--- remote/README.md
+++ remote/README.md
@@ -1,15 +1,7 @@
 # rpi-photobooth - remote control
 
-## Installation on Wemos D1 mini (v3.1.0) [ESP8266]
+- Uses MQTT notifications and gpio based push buttons
 
-Uses MQTT notifications
-Uses gpio5,6 based push buttons
-
-### Requires:
-    - ESP8266 board/library
-    - ESP8266WiFi
-    - PubSubClient
- 
 - It connects to an existing WiFi network and to a specified MQTT server, then:
     - publishes "connected" to the topic "photobooth/remote"
     - subscribes to the topic "photobooth/remote_callback"
@@ -19,27 +11,65 @@ Uses gpio5,6 based push buttons
 reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
 achieve the same result without blocking the main loop.
 
-To install the ESP8266 board, (using Arduino 1.8.10+):
+## General instructions to flash the ESP8266
 
+Flashing a ESP8266 board (using Arduino 1.8.10+)
+
+### Requirements:
+    - ESP8266 board/library
+    - ESP8266WiFi
+    - PubSubClient
+ 
+### Install Arduino
+To isntall using Arduino 1.8.10:
 - Download and install Arduino from https://www.arduino.cc/en/Main/Software
 - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
     http://arduino.esp8266.com/stable/package_esp8266com_index.json
 - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
 - Select your ESP8266 in "Tools -> Board"
-- Open "Tools → Port:xxx" and in the dropdown, select the option with "usb" in its name
+- Open "Tools → Port:xxx" and in the dropdown, select the option with "usb(serial)" in its name
 
- To install the PubSubClient library:
+### Install Libraries
+To install the PubSubClient library:
 - Open "Tools → Manage Libraries" and search for PubSubClient and install the "PubSubClient" library
 
+### Flash Board
 To connect to the board and flash via USB:
 - Downloade and install drivers from https://wiki.wemos.cc/downloads
-- Open "Tools → Board:xxx" and select WeMos D1 R2 & mini
+- Open "Tools → Board:xxx" and select the appropriate board
 - Open "Tools → Upload Speed" and select 115200
 
+### Debugging
 To read via the Serial Monitor:
     - "Open Tools → Serial Monitor" and set baudrate to 115200
     
-### Notes:
+### Notes
 - LED: LOW is the voltage level but actually the LED is on; this is because it is active low on the ESP-01
 - ON:  digitalWrite(BUILTIN_LED, LOW); 
 - OFF: digitalWrite(BUILTIN_LED, HIGH);
+
+## Specific instruction per board
+
+### Wemos D1 mini (v3.1.0)
+Wemos D1 mini (v3.1.0) [ESP8266]
+- Download and install drivers from https://wiki.wemos.cc/downloads
+- In Arduino open "Tools → Board:xxx" and select **WeMos D1 R2 & mini**
+
+Arduino IDE settings:
+ - Board: LOLIN(WEMOS) D1 R2 & mini
+ - Upload Speed: 115200
+ - CPU Frequency: 80Mhz
+ - Flash Size: 4MB (FS:2MB OTA:~1019KB)
+ - Port: tty.usbserial-1410
+
+GPIOs:
+ - 6: push button
+ - 7: led
+
+ ### ESP-01(S)
+ - Downloade and install drivers from https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver
+ - In Arduino open "Tools → Board:xxx" and select **Generic ESP8266 Module** 
+ 
+ GPIOs:
+ - 0: push button
+ - 2: led
--- remote/arduino/remote_trigger/remote_trigger.ino
+++ remote/arduino/remote_trigger/remote_trigger.ino
@@ -1,46 +1,20 @@
 /*
- Wemos D1 mini (v3.1.0) [ESP8266] MQTT notifications
- Uses gpio5,6 based push buttons
+ ESP8266 MQTT notifications for ESP-01(S)
  @author maximilian miller (miller.deutschland@gmail.com)
- @version 0.11
+ @version 0.12
 
  Requires:
- - ESP8266 board/library
- - ESP8266WiFi
- - PubSubClient
- 
- It connects to an existing WiFi network and to a specified MQTT server, then:
-  - publishes "connected" to the topic "photobooth/remote"
-  - subscribes to the topic "photobooth/remote_callback"
-    (it assumes the received payloads are strings, not binary)
+  - ESP8266 board/library
+  - ESP8266WiFi
+  - PubSubClient
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
+ Documentation:
+  https://github.com/maxmlr/rpi-photobooth/tree/master/remote
 
- To install the ESP8266 board, (using Arduino 1.8.10+):
-  - Download and install Arduino from https://www.arduino.cc/en/Main/Software
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-  - Open "Tools → Port:xxx" and in the dropdown, select the option with "usb" in its name
-
- To install the PubSubClient library:
-  - Open "Tools → Manage Libraries" and search for PubSubClient and install the "PubSubClient" library
-
- To connect to the board and flash via USB:
-  - Downloade and install drivers from https://wiki.wemos.cc/downloads
-  - Open "Tools → Board:xxx" and select WeMos D1 R2 & mini
-  - Open "Tools → Upload Speed" and select 115200
-
- To read via the Serial Monitor:
-   - "Open Tools → Serial Monitor" and set baudrate to 115200
-    
  Notes:
-  - LED: LOW is the voltage level but actually the LED is on; this is because it is active low on the ESP-01
-    ON:  digitalWrite(BUILTIN_LED, LOW); 
-    OFF: digitalWrite(BUILTIN_LED, HIGH);
+  It is important to use interupts when making network calls in the sketch.
+  If just checking the status of the button in the loop the button press
+  might be missed.
 */
 
 #include <ESP8266WiFi.h>
@@ -50,30 +24,32 @@ const char* SSID = "photobooth";
 const char* PASSWORD = "";
 const char* MQTT_SERVER = "photobooth";
 
-#define D5_BUTTON_PIN D5
-#define D6_BUTTON_PIN D6
-#define D7_LED_PIN D7
+unsigned int BUTTON_PIN;
+unsigned int LED_PIN;
 
-volatile bool D5ButtonPressedFlag = false;
-volatile bool D6ButtonPressedFlag = false;
+// ESP-01S
+static const uint8_t D3 = 0;
+static const uint8_t D4 = 2;
+#define BUTTON_PIN D3
+#define LED_PIN D4
+
+// Wemos D1
+// #define BUTTON_PIN D6
+// #define LED_PIN D7
+
+volatile bool ButtonPressedFlag = false;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long D5lastMsg = 0;
-long D6lastMsg = 0;
+long lastMsg = 0;
 int countdown = 5000;
-char msg[50];
-int value = 0;
 
 void setup_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(SSID);
-
   WiFi.begin(SSID, PASSWORD);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -104,58 +80,35 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 }
 
-ICACHE_RAM_ATTR void D5ButtonPressed() {
-  int button = digitalRead(D5_BUTTON_PIN);
+ICACHE_RAM_ATTR void ButtonPressed() {
+  int button = digitalRead(BUTTON_PIN);
   if(button == HIGH)
   {
-    D5ButtonPressedFlag = true;
+    ButtonPressedFlag = true;
   }
   return;
 }
 
-ICACHE_RAM_ATTR void D6ButtonPressed() {
-  int button = digitalRead(D6_BUTTON_PIN);
-  if(button == HIGH)
-  {
-    D6ButtonPressedFlag = true;
-  }
-  return;
-}
-
-void setup_buttons() {
-  // Initialize the buttons
-  pinMode(D5_BUTTON_PIN, INPUT);
-  pinMode(D6_BUTTON_PIN, INPUT);
-  pinMode(D7_LED_PIN, OUTPUT);
-
-  // NOTE:
-  // It is important to use interupts when making network calls in thr sketch.
-  // If just checking the status of the button in the loop the button press
-  // might be missed.
-  attachInterrupt(D5_BUTTON_PIN, D5ButtonPressed, RISING);
-  attachInterrupt(D6_BUTTON_PIN, D6ButtonPressed, RISING);
+void setup_gpios() {
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  attachInterrupt(BUTTON_PIN, ButtonPressed, RISING);
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-      // Once connected, publish a successful connection message...
       client.publish("photobooth/remote", "connected");
-      // ... and resubscribe
       client.subscribe("photobooth/remote_callback");
       led_blink(BUILTIN_LED,3,100,true);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -185,7 +138,7 @@ void setup() {
   digitalWrite(BUILTIN_LED, LOW);
   Serial.begin(115200);
   setup_wifi();
-  setup_buttons();
+  setup_gpios();
   client.setServer(MQTT_SERVER, 1883);
   client.setCallback(callback);
   digitalWrite(BUILTIN_LED, HIGH);
@@ -197,28 +150,15 @@ void loop() {
   }
   client.loop();
   long now = millis();
-  if ( D5ButtonPressedFlag ) {
-    if (now - D5lastMsg > countdown) {
-      D5lastMsg = now;
-      int trigger_state = 2;
-      snprintf (msg, 50, "trigger-%ld", trigger_state);
-      Serial.print("D5 - sending: ");
-      Serial.println(msg);
-      client.publish("photobooth/remote", msg);
-      led_blink(BUILTIN_LED,1,50,true);
-    }
-    D5ButtonPressedFlag = false;
-  }
-  
-  if ( D6ButtonPressedFlag ) {
-    if (now - D6lastMsg > countdown) {
-      D6lastMsg = now;
-      Serial.println("D6 - sending: trigger");
+  if ( ButtonPressedFlag ) {
+    if (now - lastMsg > countdown) {
+      lastMsg = now;
+      Serial.println("Butten pressed - sending: trigger");
       client.publish("photobooth/remote", "trigger");
       led_blink(BUILTIN_LED,1,50,true);
-      led_blink(D7_LED_PIN,(countdown/1000)-1,1000,false);
-      led_blink(D7_LED_PIN,5,50,false);
+      led_blink(LED_PIN,(countdown/1000)-1,1000,false);
+      led_blink(LED_PIN,5,50,false);
     }
-    D6ButtonPressedFlag = false;
+    ButtonPressedFlag = false;
   }
 }
