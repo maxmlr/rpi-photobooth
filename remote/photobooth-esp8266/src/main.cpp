7482df70c7c1d4b7fc840677afb7512cdaddc021
maxmlr <miller.deutschland@gmail.com>
2021-04-15 18:30:09 +0200

    refactoring mqtt, esp remote; updated trigger.json

--- remote/photobooth-esp8266/src/main.cpp
+++ remote/photobooth-esp8266/src/main.cpp
@@ -1,24 +1,20 @@
 /*
- ESP8266 MQTT notifications for ESP-01(S)
+ ESP8266 MQTT photobooth remote for ESP-01(S) / ESP-12 (Wemos D1 mini)
  @author maximilian miller (miller.deutschland@gmail.com)
  @version 0.12
 
- Requires:
-  - ESP8266 board/library
-  - ESP8266WiFi
-  - PubSubClient
-
- Documentation:
-  https://github.com/maxmlr/rpi-photobooth/tree/master/remote
-
- Notes:
-  It is important to use interupts when making network calls in the sketch.
-  If just checking the status of the button in the loop the button press
-  might be missed.
+ Dependencies:
+  - Platform:  espressif8266
+  - Framework: arduino
+  - Libraries:
+    + ESP8266WiFi
+    + PubSubClient
+    + JLed
 */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <jled.h>
 
 // choose REMOTE or RELAY
 #define REMOTE
@@ -33,37 +29,22 @@ unsigned int LED_PIN;
 #ifdef ARDUINO_ESP8266_ESP01
 #define BUTTON_PIN 0
 #define LED_PIN 2
+#define LED_ONBOARD 2
 #elif defined(ARDUINO_ESP8266_WEMOS_D1MINI)
 #define BUTTON_PIN D6
 #define LED_PIN D7
+#define LED_ONBOARD LED_BUILTIN
 #endif
 
-volatile bool ButtonPressedFlag = false;
-
 WiFiClient espClient;
-PubSubClient client(espClient);
+PubSubClient mqtt(espClient);
 String hostname = "photobooth-r" + String(ESP.getChipId());
-long lastMsg = 0;
-int countdown = 5000;
+JLed led = JLed(LED_PIN);
+JLed led_onboard = JLed(LED_ONBOARD).LowActive();
 
-void led_blink(uint8_t led, int count, int rate, bool inverse) {
-  for (int i = 0; i < count; i++)
-  {
-    if (inverse) {
-      digitalWrite(led, LOW);
-    } else {
-      digitalWrite(led, HIGH);
-    }
-    delay(rate);
-    if (inverse) {
-      digitalWrite(led, HIGH);
-    } else {
-      digitalWrite(led, LOW);
-    }
-    delay(rate);
-  }
-  return;
-}
+volatile bool ButtonPressedFlag = false;
+long ButtonPressedLast = 0;
+int ButtonPressedLock = 1000;
 
 void setup_wifi() {
   delay(10);
@@ -76,15 +57,13 @@ void setup_wifi() {
     delay(500);
     Serial.print(".");
   }
-
   randomSeed(micros());
-
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println(hostname);
-  led_blink(LED_BUILTIN,2,500,true);
+  led_onboard.Blink(500, 500).Repeat(2);
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -101,21 +80,30 @@ void callback(char* topic, byte* payload, unsigned int length) {
     if (value.startsWith("a")) {
         String action = value.substring(1);
         if (action == "1") {
-          digitalWrite(LED_PIN, HIGH);
+          #ifdef REMOTE
+          led.On();
+          #elif defined(RELAY)
+          digitalWrite(BUTTON_PIN, HIGH);
+          #endif
         } else if (action == "0") {
-          digitalWrite(LED_PIN, LOW); 
+          #ifdef REMOTE
+          led.Off();
+          #elif defined(RELAY)
+          digitalWrite(BUTTON_PIN, LOW);
+          #endif
         }
     } else if (value.startsWith("s")) {
       String action = value.substring(1);
-      led_blink(LED_BUILTIN,action.toInt(),100,true);
+      led_onboard.Blink(100, 100).Repeat(action.toInt());
     }
   } else if (!strcmp(topic, "photobooth/link")) {
     if (value == "discover") {
-      client.publish("photobooth/link/available", (hostname + "@" + WiFi.localIP().toString()).c_str());
+      mqtt.publish("photobooth/link/available", (hostname + "@" + WiFi.localIP().toString()).c_str());
     } 
   }
 }
 
+#ifdef REMOTE
 ICACHE_RAM_ATTR void ButtonPressed() {
   int button = digitalRead(BUTTON_PIN);
   if(button == LOW)
@@ -125,58 +113,78 @@ ICACHE_RAM_ATTR void ButtonPressed() {
   return;
 }
 
+void button_update() {
+  if ( ButtonPressedFlag ) {
+    long now = millis();
+    if (now - ButtonPressedLast > ButtonPressedLock) {
+      ButtonPressedLast = now;
+      Serial.println("Butten pressed - sending: trigger to photobooth/remote");
+      mqtt.publish("photobooth/remote", "trigger");
+    }
+    ButtonPressedFlag = false;
+  }
+}
+#endif
+
 void setup_gpios() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  #ifdef REMOTE
   pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(BUTTON_PIN, ButtonPressed, FALLING);
+  #elif defined(RELAY)
+  pinMode(BUTTON_PIN, OUTPUT);
+  #endif
 }
 
-void reconnect() {
-  while (!client.connected()) {
+void setup_mqtt() {
+  mqtt.setServer(MQTT_SERVER, 1883);
+  mqtt.setCallback(callback);
+}
+
+void mqtt_reconnect() {
+  while (!mqtt.connected()) {
     Serial.print("Attempting MQTT connection...");
-    if (client.connect(hostname.c_str())) {
+    if (mqtt.connect(hostname.c_str())) {
       Serial.println("connected");
-      client.publish("photobooth/link/register", (hostname +"/" +  WiFi.macAddress()).c_str());
-      client.subscribe("photobooth/link");
-      client.subscribe("photobooth/remote/callback");
-      client.subscribe(("photobooth/remote/" + hostname).c_str());
-      led_blink(LED_BUILTIN,3,100,true);
+      mqtt.publish("photobooth/link/register", (hostname +"/" +  WiFi.macAddress()).c_str());
+      mqtt.subscribe("photobooth/link");
+      mqtt.subscribe("photobooth/remote/callback");
+      mqtt.subscribe(("photobooth/remote/" + hostname).c_str());
+      led_onboard.Blink(100, 100).Repeat(3);
     } else {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
+      Serial.print(mqtt.state());
       Serial.println(" try again in 5 seconds");
       delay(5000);
     }
   }
 }
 
+void mqtt_update() {
+  if (!mqtt.connected()) {
+    mqtt_reconnect();
+  }
+  mqtt.loop();
+}
+
+void led_update() {
+  led.Update();
+  led_onboard.Update();
+}
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  led_onboard.On();
   Serial.begin(115200);
   setup_wifi();
   setup_gpios();
-  client.setServer(MQTT_SERVER, 1883);
-  client.setCallback(callback);
-  digitalWrite(LED_BUILTIN, HIGH);
+  setup_mqtt();
+  led_onboard.Off();
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-  long now = millis();
-  if ( ButtonPressedFlag ) {
-    if (now - lastMsg > countdown) {
-      lastMsg = now;
-      Serial.println("Butten pressed - sending: trigger");
-      client.publish("photobooth/remote", "trigger");
-      led_blink(LED_BUILTIN,1,50,true);
-      led_blink(LED_PIN,(countdown/1000)-1,1000,false);
-      led_blink(LED_PIN,5,50,false);
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    ButtonPressedFlag = false;
-  }
+  mqtt_update();
+  led_update();
+  #ifdef REMOTE
+  button_update();
+  #endif
 }
